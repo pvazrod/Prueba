52fa5a02d2779ca0abec67a3e9f54113fb0b1778
Pablo Vázquez Rodríguez <xyoshii12@gmail.com>
2021-01-19 09:02:38 +0100

    pruebamaster

---
+++ Prueba/src/pruebapaquete/PruebaClass.java
@@ -0,0 +1,13 @@
+package pruebapaquete;
+
+
+public class PruebaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Master");
+
+	}
+
+}
